395a3400d239bf21caaa03e4071a15c50eb02f9b
allenkays <allenkays@tutanota.com>
2023-01-23 04:58:56 -0500

    Create insertion sort algorithm

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+
+/**
+ * swap_list - swaps 2 nodes in a doubly-linked list
+ * @a: address of first node
+ * @b: address of second node
+ */
+void swap_list(listint_t *a, listint_t *b)
+{
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
+
+}
+
+/**
+ * insertion_sort_list - insertion sorts a doubly-linked list
+ * @list: address of pointer to head node
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *i, *j;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	i = (*list)->next;
+	while (i)
+	{
+		j = i;
+		i = i->next;
+		while (j && j->prev)
+		{
+			if ((j->prev->n) > (j->n))
+			{
+				swap_list(j->prev, j);
+				if (!j->prev)
+					*list =j;
+				print_list((const listint_t *)*list);
+			}
+			else
+				j = j -> prev;
+		}	
+	}	
+}
