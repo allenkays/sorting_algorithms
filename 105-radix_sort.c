f2089cd11517a45f46a77ec0abd53dcc509aae07
allenkays <allenkays@tutanota.com>
2023-01-24 14:52:22 -0500

    Correct undeclared function error

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,6 +1,36 @@
 #include "sort.h"
 
 /**
+ * count_sort_LSD - count sort with LSD
+ * @array: array to sort
+ * @size: size of the array
+ * @lsd: least significant digit
+ */
+void count_sort_LSD(int *array, size_t size, size_t lsd)
+{
+        int count_arr[10] = {0}, *out_arr, l, m;
+        size_t k, n;
+
+        out_arr = malloc(sizeof(int) * size);
+
+        for (k = 0; k < size; k++)
+                count_arr[(array[k] / lsd) % 10]++;
+        for (l = 1; l < 10; l++)
+                count_arr[l] += count_arr[l - 1];
+
+        for (m = size - 1; m >= 0; m--)
+        {
+                out_arr[count_arr[(array[m] / lsd) % 10] - 1] = array[m];
+                count_arr[(array[m] / lsd) % 10]--;
+        }
+
+        for (n = 0; n < size; n++)
+                array[n] = out_arr[n];
+
+        free(out_arr);
+}
+
+/**
  * radix_sort - sorts an array following the Radix sort algorithm
  * @array: array of ints to sort
  * @size: size of the array
@@ -25,32 +55,3 @@ void radix_sort(int *array, size_t size)
 	}
 }
 
-/**
- * count_sort_LSD - count sort with LSD
- * @array: array to sort
- * @size: size of the array
- * @lsd: least significant digit
- */
-void count_sort_LSD(int *array, size_t size, size_t lsd)
-{
-	int count_arr[10] = {0}, *out_arr, l, m;
-	size_t k, n;
-
-	out_arr = malloc(sizeof(int) * size);
-
-	for (k = 0; k < size; k++)
-		count_arr[(array[k] / lsd) % 10]++;
-	for (l = 1; l < 10; l++)
-		count_arr[l] += count_arr[l - 1];
-
-	for (m = size - 1; m >= 0; m--)
-	{
-		out_arr[count_arr[(array[m] / lsd) % 10] - 1] = array[m];
-		count_arr[(array[m] / lsd) % 10]--;
-	}
-
-	for (n = 0; n < size; n++)
-		array[n] = out_arr[n];
-
-	free(out_arr);
-}
