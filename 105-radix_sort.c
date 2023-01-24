76105cb99c018211558a291e7cf3cd18f74bbf21
allenkays <allenkays@tutanota.com>
2023-01-24 15:01:17 -0500

    Fix betty stlye errors

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -8,26 +8,24 @@
  */
 void count_sort_LSD(int *array, size_t size, size_t lsd)
 {
-        int count_arr[10] = {0}, *out_arr, l, m;
-        size_t k, n;
+	int count_arr[10] = {0}, *out_arr, l, m;
+	size_t k, n;
 
-        out_arr = malloc(sizeof(int) * size);
+	out_arr = malloc(sizeof(int) * size);
+	for (k = 0; k < size; k++)
+		count_arr[(array[k] / lsd) % 10]++;
+	for (l = 1; l < 10; l++)
+		count_arr[l] += count_arr[l - 1];
+	for (m = size - 1; m >= 0; m--)
+	{
+		out_arr[count_arr[(array[m] / lsd) % 10] - 1] = array[m];
+		count_arr[(array[m] / lsd) % 10]--;
+	}
 
-        for (k = 0; k < size; k++)
-                count_arr[(array[k] / lsd) % 10]++;
-        for (l = 1; l < 10; l++)
-                count_arr[l] += count_arr[l - 1];
+	for (n = 0; n < size; n++)
+		array[n] = out_arr[n];
 
-        for (m = size - 1; m >= 0; m--)
-        {
-                out_arr[count_arr[(array[m] / lsd) % 10] - 1] = array[m];
-                count_arr[(array[m] / lsd) % 10]--;
-        }
-
-        for (n = 0; n < size; n++)
-                array[n] = out_arr[n];
-
-        free(out_arr);
+	free(out_arr);
 }
 
 /**
